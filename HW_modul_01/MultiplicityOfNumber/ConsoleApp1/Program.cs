3d26975361bbbd2803ecda7239b016e6965d7d29
Roman <AverkievRE@yandex.ru>
2022-04-15 21:18:52 +0300

    no message

--- HW_modul_01/MultiplicityOfNumber/ConsoleApp1/Program.cs
+++ HW_modul_01/MultiplicityOfNumber/ConsoleApp1/Program.cs
@@ -16,22 +16,49 @@ namespace MultiplicityOfNumber
     static class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Введите целое число");
-            String str = Console.ReadLine();
-            Int32 num;
+        {  
+            Console.WriteLine("Введите целое число");              // Просим пользователя ввести число
+            String str = Console.ReadLine();                       // Ввод числа
+            Int32 num;                                             // Создаем переменную типа int
 
-            if (StringIsDigits(str))
-                num = Convert.ToInt32(str);
-            else
-                Console.WriteLine("Введено не десятичное число");
+            // Проверка введенного значения
+            if (StringIsDigits(str))                               // Введено целое число
+            {
+                num = Convert.ToInt32(str);                        // Конвертирование строки в число
+
+                //Проверка диапозона
+                if (num >= 1 && num <= 100)                        // Число соответствует диапозону
+                {
+                    // Проверка кратности числа
+                    if (num % 3 == 0 && num % 5 == 0)              // Число кратно 5 и кратно 3
+                        Console.WriteLine("Fizz Bizz");
+                    else if (num % 3 == 0)                         // Число кратно 3       
+                        Console.WriteLine("Fizz");
+                    else if (num % 5 == 0)                         // Число кратно 5
+                        Console.WriteLine("Bizz");
+                    else                                          // Число не кратно 3 и 5
+                        Console.WriteLine(num);
+                }
+                else                                              // Число не входит в диапозон
+                    Console.WriteLine
+                        (
+                        $"Число {num} не входит в диапозон от 1 до 100"
+                        );
+            }
+            else                                                  // Введено не целое число
+                Console.WriteLine("Введено не десятичное число"); // Сообщение о неправильном вводе
         }
 
-        static bool StringIsDigits(String str)
+        /// <summary>
+        /// Проверка значения на не цифровые символы
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        static Boolean StringIsDigits(String str)
         {
             foreach (var item in str)
             {
-                if (!char.IsDigit(item)) return false;
+                if (!char.IsDigit(item)) return false;            // Если в строке присутствует не числовой символ
             }
             return true;
         }
