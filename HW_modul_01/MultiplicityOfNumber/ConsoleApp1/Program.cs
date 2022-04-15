bb985de69d1f38a71b6bf64a05fd1982dc7f4d43
Roman <AverkievRE@yandex.ru>
2022-04-15 21:17:26 +0300

    project implementation MultiplicityOfNumber

--- HW_modul_01/MultiplicityOfNumber/ConsoleApp1/Program.cs
+++ HW_modul_01/MultiplicityOfNumber/ConsoleApp1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+   Пользователь вводит с клавиатуры число в диапа-
+зоне от 1 до 100. Если число кратно 3 (делится на 3 без
+остатка) нужно вывести слово Fizz. Если число кратно 5
+нужно вывести слово Buzz. Если число кратно 3 и 5 нужно
+вывести Fizz Buzz. Если число не кратно не 3 и 5 нужно
+вывести само число.
+Если пользователь ввел значение не в диапазоне от 1
+до 100 требуется вывести сообщение об ошибке.
+ */
+
+using System;
+
+namespace MultiplicityOfNumber
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            String str = Console.ReadLine();
+            Int32 num;
+
+            if (StringIsDigits(str))
+                num = Convert.ToInt32(str);
+            else
+                Console.WriteLine("Введено не десятичное число");
+        }
+
+        static bool StringIsDigits(String str)
+        {
+            foreach (var item in str)
+            {
+                if (!char.IsDigit(item)) return false;
+            }
+            return true;
+        }
+    }
+}
